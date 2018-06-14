2e7b82450b71ea38badd8636bcb8dd4654302394
icyhearts <icyhearts@163.com>
2018-06-14 20:20:57 +0800

    begin split files

--- gitadd.sh
+++ gitadd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git add gitadd.sh examples/cifar10/cifar10_quick_solver.prototxt examples/cifar10/cifar10_quick_train_test.prototxt examples/cifar10/create_cifar10.sh examples/mnist/lenet_solver.prototxt python/draw_net.py examples/cpp_classification/n1_classificationBatch.cpp
-git add tools/convert_imageset.fullList.cpp examples/cpp_classification/n2_selfClassificationBatch.cpp examples/cpp_classification/n3_classificationPipe.cpp
+git add tools/convert_imageset.fullList.cpp examples/cpp_classification/n2_selfClassificationBatch.cpp keli/src/n3_classificationPipe.cpp
---
+++ keli/src/n3_classificationPipe.cpp
@@ -0,0 +1,103 @@
+#include <caffe/caffe.hpp>
+#include<fstream>
+#include<map>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <algorithm>
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+// self
+#include"include/utils.h"
+#include"include/classify.h"
+// end of self
+using namespace caffe;  // NOLINT(build/namespaces)
+using std::string;
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using cv::Mat;
+
+
+int main(int argc, char** argv) {
+	if (argc < 6) {
+		std::cerr << "Usage: " << argv[0]
+			<< " deploy.prototxt network.caffemodel"
+			<< " mean.binaryproto labels.txt img.jpg" << std::endl;
+		return 1;
+	}
+
+	::google::InitGoogleLogging(argv[0]);
+
+	string model_file   = argv[1];
+	string trained_file = argv[2];
+	string mean_file    = argv[3];
+	string label_file   = argv[4];
+	Classifier classifier(model_file, trained_file, mean_file, label_file);
+	vector<string> mainLabels = classifier.getLabels();
+	string filelist = argv[5];
+	ifstream fin(filelist);
+	if (!fin)
+	{
+		cerr<<"parameter file does not exist."<<endl;
+		return 1;
+	}
+	//	map<string, int> imgLabelList;
+	vector<pair<string, int> > imgLabelList;
+	while(!fin.eof())
+	{
+		string str;
+		getline( fin, str );
+		if (str[0] == '#')
+		{
+			// 以‘＃’开头的是注释
+			continue;
+		}
+
+		int pos = str.find(" ");
+		if (pos == -1)
+			continue;
+		string imgName = str.substr( 0, pos );
+		string label = str.substr( pos+1, str.length() );
+		// remove spaces from string
+		imgName.erase( std::remove_if(imgName.begin(), imgName.end(), [](char x){return std::isspace(x);}),
+				imgName.end());
+		label.erase( std::remove_if(label.begin(), label.end(), [](char x){return std::isspace(x);}),
+				label.end());
+		imgLabelList.push_back(make_pair(imgName, stoi(label)) );
+		if ( !fin.good() )
+			break;
+	}
+	fin.close();
+	ofstream fout("cppPred.txt");
+	for(int i=0; i<imgLabelList.size(); ++i){
+		std::vector<cv::Mat> imgs;
+		cv::Mat img = cv::imread(imgLabelList[i].first, -1);
+		CHECK(!img.empty()) << "Unable to decode image " << imgLabelList[i].first;
+		imgs.push_back(img);
+		// all_predictions should have batchSize element: <vector<Prediction>>
+		std::vector<std::vector<Prediction> > all_predictions = classifier.Classify(imgs);
+		std::vector<Prediction>& predictions = all_predictions[0];// predictions has N element
+		Prediction p = predictions[0]; // we only need top-1
+		string predLabel = p.first;
+		string realLabel = mainLabels[ imgLabelList[i].second];
+		string imgName = imgLabelList[i].first;
+		string lineBuffer = imgName + " |" + realLabel + " |" + predLabel;
+		if(!realLabel.compare(predLabel) ){//  == =>0
+			lineBuffer += "\n";
+		}else{
+			lineBuffer += " | ERROR1\n";
+		}
+		cout<<lineBuffer;
+		fout<<lineBuffer;
+	}
+	fout.close();
+
+}
